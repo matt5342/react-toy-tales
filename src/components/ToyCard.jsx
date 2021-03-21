e13c157860777da5bd8fa6ff58067a0e55726664
Matt <mattfsewell@gmail.com>
2021-03-21 14:21:59 -0400

    commit tree 8277a61c8fad5121bdd2b07af52b61b68657cd62

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,43 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  deleteToy = id => {
+    // debugger÷\
+    fetch('http://localhost:3000/toys/' + id, { method: 'DELETE' })
+    .then(() => {
+      this.props.handleClick()
+      this.props.fetchToys()}
+      )
+  }
+
+  addLike = toy => {
+    let newLikes = toy.likes + 1
+    let likedToy = {
+      "name": toy.name, 
+      "image": toy.image, 
+      "likes": newLikes
+    }
+    let reqObj = {
+      headers: {"Content-Type": "application/json"},
+      method: "PATCH",
+      body: JSON.stringify(likedToy)
+    }
+    fetch('http://localhost:3000/toys/' + toy.id, reqObj)
+    .then(r => r.json())
+    .then(toyData => {
+      this.props.handleClick()
+      this.props.fetchToys()}
+      )
+    }
+
   render() {
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={'' /* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{this.props.toy.name}</h2>
+        <img src={this.props.toy.image} alt={this.props.toy.name} className="toy-avatar" />
+        <p>{this.props.toy.likes} Likes </p>
+        <button onClick={()=> this.addLike(this.props.toy)} className="like-btn">Like {'<3'}</button>
+        <button onClick={() => this.deleteToy(this.props.toy.id)} className="del-btn">Donate to GoodWill</button>
       </div>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  state = {}
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    let newToy = {
+      "name": this.state.name, 
+      "image": this.state.image, 
+      "likes": 0
+    }
+    let reqObj = {
+      headers: {"Content-Type": "application/json"},
+      method: "POST",
+      body: JSON.stringify(newToy)
+    }
+    fetch('http://localhost:3000/toys', reqObj)
+    .then(r => r.json())
+    .then(toyData => {
+      this.props.handleClick()
+      this.props.fetchToys()}
+      )
+  }
+  
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form onSubmit={e => this.handleSubmit(e)} className="add-toy-form">
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input onChange={e => this.handleChange(e)} type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input onChange={e => this.handleChange(e)} type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
